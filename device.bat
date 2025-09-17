aff254f25a76b7ea927bb607d386d331dbfa79c3
dimpal0506 <dimpalalokar@gmail.com>
2025-09-17 19:56:36 +0530

    Update device.bat

--- device.bat
+++ device.bat
@@ -6,7 +6,7 @@ echo Restarting ADB in TCP/IP mode on port 5555...
 adb tcpip 5555
 
 :: Directly use your phone IP
-set IP=192.168.0.106
+set IP=MOBILE_IPAddress
 
 echo Connecting to device with IP %IP%:5555 ...
 adb connect %IP%:5555
